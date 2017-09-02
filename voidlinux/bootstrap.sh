482478c03b0d4c5dfa8d0d5b9951c39d6ff8ea82
konradit <konradit@users.noreply.github.com>
2017-09-02 13:14:34 +0200

    fixed bootstrap

--- voidlinux/bootstrap.sh
+++ voidlinux/bootstrap.sh
@@ -3,13 +3,11 @@ curl https://raw.githubusercontent.com/KonradIT/dotfiles/master/bashrc >> ~/.zsh
 git clone https://github.com/konradit/compsci
 echo -ne "Enter your git token: "; read token
 sudo xbps-install alsa-plugins-pulseaudio cmus-pulseaudio cmus moc notepadqq
-https://addons.mozilla.org/firefox/downloads/file/685614/ublock_origin-1.13.8-an+fx+sm+tb.xpi ublock.xpi
-wget https://addons.mozilla.org/firefox/downloads/latest/vimfx/addon-404785-latest.xpi vimfx.xpi
+wget https://addons.mozilla.org/firefox/downloads/file/685614/ublock_origin-1.13.8-an+fx+sm+tb.xpi -o ublock.xpi
+wget https://addons.mozilla.org/firefox/downloads/latest/vimfx/addon-404785-latest.xpi -o vimfx.xpi
 gksudo firefox -install-global-extension ublock.xpi
 gksudo firefox -install-global-extension vimfx.xpi
 wget https://github.com/KonradIT/dotfiles/blob/master/wall/main.png
 feh --bg-scale main.png
 ~/.zshrc
 echo "Finished. Close this terminal and open a new one".
-
-
