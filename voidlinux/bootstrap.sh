5a1e80544d7919aec7232c42b59454a1ce6f971c
KonradIT <github@chernowii.com>
2017-09-02 19:39:43 +0100

    simplified bootstrap.sh

--- voidlinux/bootstrap.sh
+++ voidlinux/bootstrap.sh
@@ -1,13 +1,7 @@
+### General voidrice post-installation script ###
+
 cat ~/.zshrc | sed "s/nik123true/void/g" > ~/.bashrc
 curl https://raw.githubusercontent.com/KonradIT/dotfiles/master/bashrc >> ~/.zshrc
-git clone https://github.com/konradit/compsci
-echo -ne "Enter your git token: "; read token
-sudo xbps-install alsa-plugins-pulseaudio cmus-pulseaudio cmus moc notepadqq
-wget https://addons.mozilla.org/firefox/downloads/file/685614/ublock_origin-1.13.8-an+fx+sm+tb.xpi -o ublock.xpi
-wget https://addons.mozilla.org/firefox/downloads/latest/vimfx/addon-404785-latest.xpi -o vimfx.xpi
-gksudo firefox -install-global-extension ublock.xpi
-gksudo firefox -install-global-extension vimfx.xpi
-wget https://github.com/KonradIT/dotfiles/blob/master/wall/main.png
-feh --bg-scale main.png
-~/.zshrc
+sudo xbps-install alsa-plugins-pulseaudio cmus-pulseaudio cmus moc wget curl
+feh --bg-scale ../wall/main.png
 echo "Finished. Close this terminal and open a new one".
